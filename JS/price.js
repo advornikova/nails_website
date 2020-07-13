ff73250bc3c6028eb5e7f972168416779e75a757
Anastasia Dvornikova <dvornikovaan@gmail.com>
2020-07-12 22:41:47 -0400

    Added images, price.js

---
+++ JS/price.js
@@ -0,0 +1,31 @@
+let priceList = [
+    {
+        id: 'm_basic',
+        price: 30
+    },
+    {
+        id:'m_gel',
+        price: 40
+    },
+    {
+        id: 'p_basic',
+        price: 40
+    },
+    {
+        id: 'p_gel',
+        price: 50
+    },
+    {
+        id: 'easy',
+        price: 5
+    },
+    {
+        id: 'complex',
+        price: 10
+    },
+    {
+        id: 'complex',
+        price: 15
+    }
+    
+]
